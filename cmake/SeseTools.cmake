d19770b913c958c942a0409a34589ed2a2604858
Kaoru <shiinasama2001@gmail.com>
2024-02-15 03:57:59 +0800

    修复配置错误

--- cmake/SeseTools.cmake
+++ cmake/SeseTools.cmake
@@ -57,43 +57,42 @@ macro(SESE_ADD_LIBRARY TARGET_NAME)
 endmacro()
 
 macro(SESE_AUTO_FIND_VCPKG)
-    if (NOT $ENV{VCPKG_ROOT} STREQUAL "")
-        message(STATUS "VCPKG_ROOT :$ENV{VCPKG_ROOT}")
-        include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
-        return()
-    endif ()
-
-    if (CMAKE_TOOLCHAIN_FILE)
+    set(VCPKG_FOUND OFF)
+    if (DEFINED CMAKE_TOOLCHAIN_FILE)
         get_filename_component(FILENAME ${CMAKE_TOOLCHAIN_FILE} NAME)
         if (${FILENAME} STREQUAL "vcpkg.cmake")
-            return()
+            set(VCPKG_FOUND ON)
         endif ()
     endif ()
 
-    find_path(VCPKG_TOOLCHAIN_FILE vcpkg.cmake
-            C:/src/vcpkg/scripts/buildsystems
-            C:/vcpkg/scripts/buildsystems
-            D:/vcpkg/scripts/buildsystems
-            E:/vcpkg/scripts/buildsystems
-            F:/vcpkg/scripts/buildsystems
-            C:/Users/$ENV{USER}/vcpkg/scripts/buildsystems
-            C:/Users/$ENV{USER}/.vcpkg/scripts/buildsystems
-            /usr/local/vcpkg/scripts/buildsystems
-            /src/vcpkg/scripts/buildsystems
-            /home/$ENV{USER}/vcpkg/scripts/buildsystems
-            /home/$ENV{USER}/.vcpkg/scripts/buildsystems
-            /Users/$ENV{USER}/vcpkg/scripts/buildsystems
-            /Users/$ENV{USER}/.vcpkg/scripts/buildsystems
-    )
+    if (NOT VCPKG_FOUND)
+        if (NOT $ENV{VCPKG_ROOT} STREQUAL "")
+            set(VCPKG_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+            message(STATUS "VCPKG_TOOLCHAIN_FILE :${VCPKG_TOOLCHAIN_FILE}")
+            include(${VCPKG_TOOLCHAIN_FILE})
+        else ()
+            find_path(VCPKG_TOOLCHAIN_DIR vcpkg.cmake
+                    C:/src/vcpkg/scripts/buildsystems
+                    C:/vcpkg/scripts/buildsystems
+                    D:/vcpkg/scripts/buildsystems
+                    E:/vcpkg/scripts/buildsystems
+                    F:/vcpkg/scripts/buildsystems
+                    C:/Users/$ENV{USER}/vcpkg/scripts/buildsystems
+                    C:/Users/$ENV{USER}/.vcpkg/scripts/buildsystems
+                    /usr/local/vcpkg/scripts/buildsystems
+                    /src/vcpkg/scripts/buildsystems
+                    /home/$ENV{USER}/vcpkg/scripts/buildsystems
+                    /home/$ENV{USER}/.vcpkg/scripts/buildsystems
+                    /Users/$ENV{USER}/vcpkg/scripts/buildsystems
+                    /Users/$ENV{USER}/.vcpkg/scripts/buildsystems
+            )
 
-    if (${VCPKG_TOOLCHAIN_FILE} STREQUAL "")
-        message(FATAL_ERROR "Could not found the vcpkg.cmake")
-    else ()
-        message(STATUS "VCPKG_TOOLCHAIN_FILE :${VCPKG_TOOLCHAIN_FILE}/vcpkg.cmake")
+            if (${VCPKG_TOOLCHAIN_DIR} STREQUAL "")
+                message(FATAL_ERROR "Could not found the vcpkg.cmake")
+            else ()
+                message(STATUS "VCPKG_TOOLCHAIN_FILE :${VCPKG_TOOLCHAIN_DIR}/vcpkg.cmake")
+                include("${VCPKG_TOOLCHAIN_DIR}/vcpkg.cmake")
+            endif ()
+        endif ()
     endif ()
-
-    if (CMAKE_TOOLCHAIN_FILE)
-        set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${CMAKE_TOOLCHAIN_FILE})
-    endif ()
-    include("${VCPKG_TOOLCHAIN_FILE}/vcpkg.cmake")
 endmacro()
