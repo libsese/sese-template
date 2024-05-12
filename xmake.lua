d01ca07315b7c366679b7dd29ce7714b39136128
Kaoru <shiinasama2001@gmail.com>
2024-05-12 22:21:44 +0800

    更新核心库版本至 2.1.0

--- xmake.lua
+++ xmake.lua
@@ -4,23 +4,23 @@ local registries = {
     {
         kind = "git",
         repository = "https://github.com/libsese/vcpkg-registry",
-        baseline = "0113f7c41fd1444ecc975b49c1f750fe21c9118d",
+        baseline = "43ed8eab2f54413ebd406ca2f9fcf17240995335",
         packages = {
-            "sese-core"
+            "sese"
         }
     }
 }
 
-add_requires("vcpkg::sese-core >=2.0.1", {configs = { shared = true, baseline = "c8696863d371ab7f46e213d8f5ca923c4aef2a00",registries = registries}})
+add_requires("vcpkg::sese >=2.1.0", {configs = { shared = true, baseline = "c8696863d371ab7f46e213d8f5ca923c4aef2a00",registries = registries}})
 
 set_languages("c++17")
 
 target("template")
     set_kind("binary")
     add_files("src/*.cpp")
-    add_packages("vcpkg::sese-core")
+    add_packages("vcpkg::sese")
 
 target("unittest")
     set_kind("binary")
     add_files("test/*.cpp")
-    add_packages("vcpkg::sese-core")
+    add_packages("vcpkg::sese")
